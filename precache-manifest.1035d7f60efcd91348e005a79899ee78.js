df18e4c0a28c811ca9fdc3c60f054c514c411ad1
ltelisabeth <18218032@std.stei.itb.ac.id>
2021-09-01 00:19:51 +0700

    Updates

---
+++ precache-manifest.1035d7f60efcd91348e005a79899ee78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ab27433b7946b6ae6403e5205149e60",
+    "url": "/react_progate/index.html"
+  },
+  {
+    "revision": "24ff5b65908778763806",
+    "url": "/react_progate/static/css/main.d1df7b01.chunk.css"
+  },
+  {
+    "revision": "095a768c626eaaacd148",
+    "url": "/react_progate/static/js/2.e2a20642.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_progate/static/js/2.e2a20642.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24ff5b65908778763806",
+    "url": "/react_progate/static/js/main.e177871d.chunk.js"
+  },
+  {
+    "revision": "2cf7a07b70c6e30092fe",
+    "url": "/react_progate/static/js/runtime-main.dfab769e.js"
+  }
+]);
